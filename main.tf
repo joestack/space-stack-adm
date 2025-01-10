bca7fc793b366867c43d5407fd9e7a1c85d8b508
JoeStack <joern@tel2ip.net>
2025-01-10 17:46:52 +0100

    add md_space 2.1

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "mondoo_registration_token" "token" {
 output "space_registration_token" {
   description = "registration tokens for the specified spaces"
   value = mondoo_registration_token.token.result
-  sensitive = false
+  sensitive = true
 }
 
 ### GITHUB ###
